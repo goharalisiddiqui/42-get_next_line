9ae0fcb9223720741bece7a84029b10b24c7f425
Gohar ali Siddiqui <gsiddiqu@3-F-3.42heilbronn.de>
2021-06-30 19:39:40 +0200

    All green without norminette

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsiddiqu <gsiddiqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/27 17:45:42 by gsiddiqu          #+#    #+#             */
+/*   Updated: 2021/06/29 16:49:19 by gsiddiqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef struct s_vlist
+{
+	int				idt;
+	char			*content;
+	struct s_vlist	*next;
+}	t_vlist;
+
+int		get_next_line(int fd, char **line);
+int		ft_strlen(char *str);
+char	*ft_strdup(const char *s1);
+void	ft_vlstadd(t_vlist **lst, int id, char *str);
+t_vlist	*ft_vlstlocdel(t_vlist **vault, int fd, int ident);
+char	*ft_strchr(const char *s, int c);
+
+#endif
